3a52ad169d3e8e84114a4ec80dc51eb90d259ebd
ben hockey <neonstalwart@gmail.com>
2012-03-29 15:27:47 -0400

    build: snapshot of roughly what a profile would look like

--- build.profile.js
+++ build.profile.js
@@ -1,3 +1,40 @@
+require({
+	// configure loader to find twine to make loading the transform easier
+	packages: [
+		{
+			name: 'twine',
+			// XXX: once http://bugs.dojotoolkit.org/ticket/15103 is fixed this
+			// line can become:
+			//	location: '../exteranl/twine',
+			location: require.nodeRequire('path').dirname(process.argv[1]) + '/../external/twine',
+			main: 'Twine'
+		}
+	]
+});
+
+
+// this block is how we get dojo's build tool to use twine's transform
+require([
+	'build/buildControlDefault'
+], function (bc) {
+	// make the twine transform available - depends on config in previous block
+	bc.transforms.twine = ['twine/build/transform', 'ast'];
+
+	// add the twine transform to all transformJobs that have depsScan
+	bc.transformJobs.forEach(function (transformJob) {
+		var transforms = transformJob[1],
+			index = transforms.indexOf('depsScan');
+
+		// if 'depsScan' is in the list of transforms then...
+		if (~index) {
+			// add the twine transform before depsScan
+			transforms.splice(index, 0, 'twine');
+		}
+	});
+});
+
+
+
 var profile = {
 	releaseDir: 'release',
 
